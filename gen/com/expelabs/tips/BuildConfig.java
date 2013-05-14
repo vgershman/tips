961eaa32882821bb9153fdc11d56980f10044707
vgershman <herrschmann@gmail.com>
2013-05-15 00:56:00 +0400

    update

---
+++ gen/com/expelabs/tips/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package com.expelabs.tips;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
